3628c43fc5057c6de1d133ff6a665c50ed28a1c6
Piero Giusti <pierophp@gmail.com>
2023-01-05 10:34:43 -0300

    Progress

--- src/app/progress-test/progress-test.component.ts
+++ src/app/progress-test/progress-test.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, Observable, switchMap, tap } from 'rxjs';
+
+interface Download {
+  name: string;
+  progress: number;
+}
 
 @Component({
   selector: 'app-progress-test',
   templateUrl: './progress-test.component.html',
-  styleUrls: ['./progress-test.component.scss']
+  styleUrls: ['./progress-test.component.scss'],
 })
 export class ProgressTestComponent {
+  downloads$: Observable<Download[]>;
 
+  constructor() {
+    const download: () => Promise<Download[]> = async () => {
+      return [
+        {
+          name: 'Teste',
+          progress: 0,
+        },
+      ];
+    };
+
+    let progress = 0;
+
+    this.downloads$ = new Observable<Download[]>((observer) => {
+      const pump = () => {
+        progress += 2;
+
+        observer.next([
+          {
+            name: 'Teste',
+            progress: progress,
+          },
+        ]);
+
+        setTimeout(pump, 500);
+      };
+
+      pump();
+    });
+
+    // this.downloads$ = from(download()).pipe(
+    //   tap((downloads) => {
+    //     return downloads;
+    //   })
+    // );
+    // setTimeout(() => {
+    //   this.progress = 10;
+    // }, 2000);
+  }
 }
