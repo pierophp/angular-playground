d53c00358ad85383176e7bafc244a1f3bd82915c
Piero Giusti <pierophp@gmail.com>
2023-01-10 13:54:39 -0300

    Add HMR - Angular 11+

--- 15/src/main.ts
+++ 15/src/main.ts
@@ -1,7 +1,15 @@
+import { ɵresetCompiledComponents } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+// https://stackoverflow.com/questions/65025741/running-ng-server-with-hmr-still-causes-page-to-reload-on-changes
+declare let module: any;
+if (module.hot) {
+  module.hot.accept();
+  module.hot.dispose(() => ɵresetCompiledComponents());
+}
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
